8050d8b233c55047598246c4ef15b4086cb3bb90
daneil <B045_06@LAPTOP-O459G1GC>
2024-10-15 08:57:47 +0200

    primo commit

---
+++ src/farlocco/Salutatore.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package farlocco;
+
+/**
+ *
+ * @author B045_06
+ */
+public class Salutatore {
+    
+}
